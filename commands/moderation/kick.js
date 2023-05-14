a0b62b814ac5048c9fbb35b0ba863aa9ef8a071b
TheGiantNinja <yasserfedsi34@gmail.com>
2023-05-15 01:50:18 +0200

    Added kick command

---
+++ commands/moderation/kick.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('kick')
+        .setDescription('Select a member and kick them.')
+        .addUserOption(option => 
+                option.setName('target')
+                    .setDescription('The member to kick')
+                    .setRequired(true)
+            )
+        .addStringOption(option => 
+                option.setName('reason')
+                    .setDescription('The reason for kicking')
+            )
+        .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers)
+        .setDMPermission(false),
+    async execute(interaction) {
+            const target = interaction.options.getUser('target');
+            const reason = interaction.option.getString('reason') ?? 'No reason provided';
+    
+            await interaction.reply(`Kicked ${target.username} for reason: ${reason}`);
+            await interaction.guild.members.kick(target);
+        }
+}
