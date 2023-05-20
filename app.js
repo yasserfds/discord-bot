d83784a07d533d4c0d6a62f9e5ff37b972e13234
TheGiantNinja <yasserfedsi34@gmail.com>
2023-05-21 00:30:02 +0200

    some changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, ActivityType } = require('discord.js');
-const { token } = require('./config.json');
+const { token, MongoDB_URI } = require('./config.json');
 const mongoose = require('mongoose');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
