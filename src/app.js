ec22e426863a426400f9bda935d94cfa5eb74a1e
TheGiantNinja <yasserfedsi34@gmail.com>
2023-05-29 01:44:47 +0200

    New src code

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const { token } = require('../config.json')
+const { Client, IntentsBitField } = require('discord.js');
+const eventHandler = require('./handlers/eventHandler');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+eventHandler(client);
+
+client.login(token);
