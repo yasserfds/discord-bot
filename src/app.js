06442966f899a9469771461152003a8798ccbb66
TheGiantNinja <yasserfedsi34@gmail.com>
2023-06-04 17:40:57 +0200

    modified the file structure

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { Client, IntentsBitField } = require('discord.js');
+const mongoose = require('mongoose');
 const eventHandler = require('./handlers/eventHandler');
-const { default: mongoose } = require('mongoose');
 
 const client = new Client({
   intents: [
@@ -13,14 +13,16 @@ const client = new Client({
   ],
 });
 
-(async() => {
+(async () => {
   try {
-     mongoose.set('strictQuery', false);
+    mongoose.set('strictQuery', false);
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log('Connecte to DB.');
-    client.login(process.env.TOKEN);
+    console.log('Connected to DB.');
+
     eventHandler(client);
+
+    client.login(process.env.TOKEN);
   } catch (error) {
-    console.log(`Error: ${error}`)
+    console.log(`Error: ${error}`);
   }
 })();
