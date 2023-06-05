d40887027aaa5885dfcabf9972b8253cc23dd841
TheGiantNinja <yasserfedsi34@gmail.com>
2023-06-05 03:37:08 +0200

    Added status

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,3 +1,17 @@
+const { ActivityType } = require('discord.js')
+
+let status = [
+    {
+        name: 'Youtube',
+        type: ActivityType.Watching
+    }
+]
+
 module.exports = (client) => {
     console.log(`${client.user.tag} is online.`);
+
+    setInterval(() => {
+        let random = Math.floor(Math.random() * status.length);
+        client.user.setActivity(status[random])
+    }, 1000)
 };
