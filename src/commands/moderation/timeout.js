8577f488bcf214bbd4bd3c8a826ce8ef1970f67d
TheGiantNinja <yasserfedsi34@gmail.com>
2023-06-09 00:52:45 +0200

    Update timeout.js

--- src/commands/moderation/timeout.js
+++ src/commands/moderation/timeout.js
@@ -2,6 +2,31 @@ const { Client, Interaction, ApplicationCommandOptionType, PermissionFlagsBits }
 const ms = require('ms');
 
 module.exports = {
+
+  name: 'timeout',
+  description: 'Timeout a user.',
+  options: [
+    {
+      name: 'target-user',
+      description: 'The user you want to timeout.',
+      type: ApplicationCommandOptionType.Mentionable,
+      required: true,
+    },
+    {
+      name: 'duration',
+      description: 'Timeout duration (30m, 1h, 1 day).',
+      type: ApplicationCommandOptionType.String,
+      required: true,
+    },
+    {
+      name: 'reason',
+      description: 'The reason for the timeout.',
+      type: ApplicationCommandOptionType.String,
+    },
+  ],
+  permissionsRequired: [PermissionFlagsBits.MuteMembers],
+  botPermissions: [PermissionFlagsBits.MuteMembers],
+
   /**
    *
    * @param {Client} client
@@ -67,28 +92,4 @@ module.exports = {
       console.log(`There was an error when timing out: ${error}`);
     }
   },
-
-  name: 'timeout',
-  description: 'Timeout a user.',
-  options: [
-    {
-      name: 'target-user',
-      description: 'The user you want to timeout.',
-      type: ApplicationCommandOptionType.Mentionable,
-      required: true,
-    },
-    {
-      name: 'duration',
-      description: 'Timeout duration (30m, 1h, 1 day).',
-      type: ApplicationCommandOptionType.String,
-      required: true,
-    },
-    {
-      name: 'reason',
-      description: 'The reason for the timeout.',
-      type: ApplicationCommandOptionType.String,
-    },
-  ],
-  permissionsRequired: [PermissionFlagsBits.MuteMembers],
-  botPermissions: [PermissionFlagsBits.MuteMembers],
 };
